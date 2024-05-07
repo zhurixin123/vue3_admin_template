408bd9cc91e7c62d6e34242fea0b1f2f8f960611
zhurixin <878899182@qq.com>
2024-05-07 22:40:37 +0800

    提交格式化代码

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
-  <div class="box">
-    <h1>App根组件</h1>
+  <div>
+
   </div>
 </template>
 
 <script setup lang="ts">
-const str = '我爱你祖国'
+for (let i = 0; i < 100; i++)
+{
+  console.log(i);
+}
 </script>
 
-<style scoped lang="scss">
-.box {
-  width: 600px;
-  height: 400px;
-  background-color: black;
-  h1 {
-    color: white;
-  }
-}
+<style scoped>
+
 </style>
